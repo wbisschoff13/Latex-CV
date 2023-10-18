d388ce1195c3c3d7f89bde0883f7dfbd24cee6e7
Werner Bisschoff <wbisschoff13@gmail.com>
2023-10-18 19:56:21 +0000

    Fix docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+
+RED='\033[0;31m'
+NC='\033[0m'
+
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 SCRIPT_PARENT_DIR="$SCRIPT_DIR/.."
 cd "$SCRIPT_PARENT_DIR" || exit
 
 TAG="latex-cv"
-OUTPUT_DIR="out"
+OUTPUT_DIR="/workdir/out/."
 
-docker build -t $TAG -f "docker/Dockerfile" . || exit
+docker build \
+    -t "$TAG" \
+    -f "docker/Dockerfile" \
+    . ||
+    {
+        echo "Failed to build docker image"
+        exit 1
+    }
 
-container_id=$(docker create $TAG)
+docker cp "$(
+    docker create "$TAG"
+):$OUTPUT_DIR" .
 
-docker cp "$container_id:$OUTPUT_DIR" $OUTPUT_DIR/ || exit
-docker stop "$container_id" || exit
-docker rm "$container_id" || exit
+docker rm "$(
+    docker stop "$(
+        docker ps -a -q --filter ancestor="$TAG"
+    )" >/dev/null 2>&1
+)" >/dev/null 2>&1
